fbc5fba268ee93d9886f4d910c24eacee5f9bf9f
mzavalaf <35230769+mzavalaf@users.noreply.github.com>
2018-04-15 16:57:38 -0600

    gdgdg

--- assigments/lesson-7/scripts/windchill.js
+++ assigments/lesson-7/scripts/windchill.js
@@ -19,3 +19,17 @@
   document.getElementById('wind-direction').innerHTML = windDirection;
   document.getElementById('chill-factor').innerHTML = chillFactor;
 })();
+function doInputOutput () 
+{
+    var temp = parseInt (document.getElementById("tempInputBox").value);
+    var wSpeed = parseInt (document.getElementById("wSpeedInputBox").value);
+	
+	// rounded to the nearest whole number and added degrees F.
+    var result = windChill (temp, wSpeed);
+    document.getElementById("output").innerHTML = result.toFixed(0) + " &#x2109";	
+}    
+function windChill (temp, wSpeed)
+{
+    var f = (35.74 + 0.6215 * temp - 35.75 * Math.pow(wSpeed, 0.16) + 0.4275 * temp * Math.pow(wSpeed, 0.16));
+    return f;	
+}
